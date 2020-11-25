cf1393a24416603f128c5e8d98252fd71169aa78
Dom Sleightholme <domsleightholme7@gmail.com>
2020-11-25 21:54:20 +0000

    UI and Cursor done

---
+++ COMP Game/Project Folder/Project/Coop Party Game/Assets/Scripts/MenuCursor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCursor : MonoBehaviour
+{
+    float depth = 10.0f;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        FollowMouse();
+    }
+
+    void FollowMouse()
+    {
+       var mousePos = Input.mousePosition;
+       var wantedPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, depth));
+       transform.position = wantedPos;
+    }
+}
