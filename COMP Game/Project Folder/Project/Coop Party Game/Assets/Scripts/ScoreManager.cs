b0dac06696010457c8b6b4eddb3867b16b4a8358
Dom Sleightholme <domsleightholme7@gmail.com>
2021-01-10 17:40:22 +0000

    Controller Mapped for Obstacle Course

--- COMP Game/Project Folder/Project/Coop Party Game/Assets/Scripts/ScoreManager.cs
+++ COMP Game/Project Folder/Project/Coop Party Game/Assets/Scripts/ScoreManager.cs
@@ -5,15 +5,11 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public Text Player1Score;
-    public Text Player2Score;
-
     public int Player1;
     public int Player2;
 
     public void Update()
     {
-        Player1Score.text = Player1.ToString();
-        Player2Score.text = Player2.ToString();
+
     }
 }
